92d224d4018ad64f8874d2c347139145400d3d00
Razvan Dumitriu <razvan@dumitriu.co.uk>
2022-12-19 13:04:41 +0200

    Delete review functionality

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StoreProject.Data;
 using StoreProject.Models;
 
@@ -41,6 +42,37 @@ namespace StoreProject.Controllers
             {
                 TempData["Error"] = "A aparut o eroare, va rugam reincercati.";
                 return RedirectToAction("Show", "Products", new { id = id });
+            }
+        }
+
+        [Authorize(Roles = "User,Colaborator,Admin")]
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            string userID = _userManager.GetUserId(User);
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            else if (User.IsInRole("Admin") || userID == review.UserID)
+            {
+                try
+                {
+                    db.Remove(review);
+                    db.SaveChanges();
+                    TempData["Success"] = "Recenzie stearsa cu succes!";
+                    return RedirectToAction("Index", "Products");
+                }
+                catch
+                {
+                    TempData["Error"] = "A aparut o eroare, va rugam reincercati.";
+                    return RedirectToAction("Index", "Products");
+                }
+            }
+            else
+            {
+                return Unauthorized();
             }
         }
     }
